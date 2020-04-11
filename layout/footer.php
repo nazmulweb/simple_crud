0e6163a8b7f5bac393537416940a51992f2f2150
raihan <raihan@sarosit.com>
2020-04-12 04:30:24 +0600

    ceated header and footer

---
+++ layout/footer.php
@@ -0,0 +1,17 @@
+    <footer class="card-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <p>Developed by <a href="www.nazmulhasan.info">Nazmul Hasan</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header class="bg-dark header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center d-block">
+                    <a href="index.php" class="d-block"> Simple CRUD </a>
+                </div>
+            </div>
+        </div>
+    </header>
